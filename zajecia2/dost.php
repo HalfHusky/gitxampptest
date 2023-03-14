9785627f2182229af3925117b14c03375db4322f
kris <kklemenski@edu.cdv.pl>
2023-03-14 17:20:04 +0100

    zaj2

---
+++ zajecia2/dost.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista</title>
+</head>
+<body>
+    <ul type="square">
+        <LI>Wielkopolska</LI>
+            <ol type="1">
+                <LI>Poznan</LI>
+                <li>Gniezno</li>
+            
+            </ol>
+        <LI>Dolnoslaskie</LI>
+        <LI>ZachodioPomorskie</LI>
+        <ol>
+            <li>Stargrad</li>
+        </ol>
+    </ul>
+
+    <?php
+        include "./list.php";
+        //include_once "./list.php";
+        //require "./list.php"; 
+        // @- ignorowanie bledow
+    
+    ?>
+<h4>Kod po Skrypt</h4>
+
+
+
+
+</body>
+</html>
+
+
---
+++ zajecia2/list.php
@@ -0,0 +1,11 @@
+<?php
+    echo <<<LIST
+    <h4>Lista</h4>
+    <ol>
+        <li>czerwony</li>
+        <li>niebieski</li>
+        <li>zielony</li>
+    </ol>
+
+    LIST;
+
